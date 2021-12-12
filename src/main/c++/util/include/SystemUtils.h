36c358a0d986784265aa66c324cebd8df9ea9c13
andreas-grimm <andreas.grimm@gricom.eu>
2021-12-12 23:32:17 +0100

    First CMake Test working for static library

--- src/main/c++/util/include/SystemUtils.h
+++ src/main/c++/util/include/SystemUtils.h
@@ -2,13 +2,15 @@
 // Created by Andreas Grimm on 09/11/2021.
 //
 
-#ifndef GD_BASIC_RUNTIME_SYSTEMUTILS
-#define GD_BASIC_RUNTIME_SYSTEMUTILS
-
+#ifndef GD_BASIC_RUNTIME_SYSTEM_UTILS
+#define GD_BASIC_RUNTIME_SYSTEM_UTILS
 
 class SystemUtils {
-
+    public:
+        SystemUtils() = default;
+        ~SystemUtils() = default;
+        static unsigned long getFreeMemory();
+        static unsigned long long getTotalSystemMemory();
 };
 
-
-#endif //GD_BASIC_RUNTIME_SYSTEMUTILS
+#endif //GD_BASIC_RUNTIME_SYSTEM_UTILS
